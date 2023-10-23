27b8e84edc02ab603fded4cf2a1e5ea64e259c6d
Sloana <hajderal@lsbu.ac.uk>
2023-10-23 10:49:06 +0100

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -11,12 +11,9 @@ import pandas as pd
 from io import StringIO
 path="https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"    
 
-headers = {
-"key": "x-api-key",
-"value": "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"
-}
+headers = {"x-api-key":"yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}
 url=" https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
-url2="https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details"
+url2="https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/1"
 #Create class DataExtrator to extract data from tables RDS database provided
 class DataExtrator:
     # the method that extract database table to a pandas dataframe
@@ -35,7 +32,7 @@ class DataExtrator:
         tabula.convert_into("https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf", "C:/Users/laura/OneDrive/Desktop/MRDC/output.csv", output_format="csv", pages='all')
         dfs2= pd.read_csv('C:/Users/laura/OneDrive/Desktop/MRDC/output.csv')
         return dfs2
-    ##############------------------ask for help------------------------------------
+    ##############-------------------Tomorrow ask for help------------------------------------
     # the method that return the number of stores
     def list_number_of_stores(url, headers):
         response = requests.get(url, headers=headers)
@@ -44,11 +41,13 @@ class DataExtrator:
 
     # the method that return the store data
     def retrieve_stores_data(url2):
-        response = requests.get(url2)
-        # print(response.status_code)
-        repos = response.json()
-        # print(repos.keys())
-        return repos
+        data_store=[]
+        for i in range((DataExtrator.list_number_of_stores(url, headers))['number_stores']):
+            response = requests.get("https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/%s"%i, headers=headers)
+            data_store.append(response.json())
+        df=pd.DataFrame.from_dict(data_store) 
+        print(df)
+        return df
 
     def extract_from_s3(bucket, key):
         s3 = boto3.client('s3')
@@ -64,53 +63,35 @@ class DataExtrator:
         j = json.loads(obj['Body'].read())
         return j
     
-# print(DataExtrator.list_db_tables(DatabaseConnector.init_db_engine()))
-# ### ------------------------UPLOAD user data in databse----------------------------------------------
-# DatabaseConnector.upload_to_db(DataCleaning.standardise_phone_number(DataCleaning.clean_user_data(DataExtrator.read_rds_table('legacy_users')).phone_number), 'dim_users') 
 
-# ### ------------------------UPLOAD card data in databse----------------------------------------------
-# path="https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf" 
+# ### ------------------------UPLOAD user data in database----------------------------------------------
+DatabaseConnector.upload_to_db(DataCleaning.standardise_phone_number(DataCleaning.clean_user_data(DataExtrator.read_rds_table('legacy_users')).phone_number), 'dim_users') 
 
-# DatabaseConnector.upload_to_db(DataCleaning.clean_card_data(DataExtrator.retrieve_pdf_data(path)), 'dim_card_details') 
-# ### ------------------------UPLOAD product data in databse----------------------------------------------
-# bucket = 'data-handling-public'
-# key = 'products.csv'
-# DatabaseConnector.upload_to_db(DataCleaning.clean_product_data(DataCleaning.convert_product_weights(DataExtrator.extract_from_s3(bucket, key))), 'dim_products')
+# ### ------------------------UPLOAD card data in database----------------------------------------------
+path="https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf" 
+
+DatabaseConnector.upload_to_db(DataCleaning.clean_card_data(DataExtrator.retrieve_pdf_data(path)), 'dim_card_details') 
 
 
-# ### ------------------------UPLOAD order data in databse----------------------------------------------
 
-# DatabaseConnector.upload_to_db(DataCleaning.clean_order_data(DataExtrator.read_rds_table('orders_table')), 'orders_table')
+# ### ------------------------UPLOAD store data in database----------------------------------------------
+DatabaseConnector.upload_to_db(DataCleaning.called_clean_store_data(DataExtrator.retrieve_stores_data(url2)),'dim_store_details')
 
-
-# ### ------------------------UPLOAD JSON data in databse----------------------------------------------
-# DatabaseConnector.upload_to_db(DataCleaning.clean_json(DataExtrator.extrac_json()),'dim_date_times')
-
-
-print(DataExtrator.list_number_of_stores(url, headers))
-print(DataExtrator.retrieve_stores_data(url2))
+# ### ------------------------UPLOAD product data in database----------------------------------------------
+bucket = 'data-handling-public'
+key = 'products.csv'
+DatabaseConnector.upload_to_db(DataCleaning.clean_product_data(DataCleaning.convert_product_weights(DataExtrator.extract_from_s3(bucket, key))), 'dim_products')
 
 
-# # Task 4 Milestone 2
-# # print(DataExtrator.retrieve_pdf_data(path))
-# #Data extracted and cleaned will be saved in the local database named sale_data in the localhost
-# # DataExtrator.list_number_of_stores(url,headers)
-# # DataExtrator.retrieve_stores_data(url2)
+# ### ------------------------UPLOAD order data in database----------------------------------------------
 
-# # upload user details in local database
+DatabaseConnector.upload_to_db(DataCleaning.clean_order_data(DataExtrator.read_rds_table('orders_table')), 'orders_table')
 
 
-#
+# ### ------------------------UPLOAD JSON data in database----------------------------------------------
+DatabaseConnector.upload_to_db(DataCleaning.clean_json(DataExtrator.extrac_json()),'dim_date_times')
 
 
-# DatabaseConnector.upload_to_db(df,'dim_date_times') 
 
-# print(DataCleaning.convert_product_weights(DataExtrator.extract_from_s3(bucket, key)))  
 
-# print(DataCleaning.clean_user_data(DataExtrator.read_rds_table('legacy_users')))
-# df=DataCleaning.clean_user_data(DataExtrator.read_rds_table('legacy_users'))
-
-# phone_number=df.phone_number
-# df.phone_number=DataCleaning.standardise_phone_number(DataCleaning.clean_user_data(DataExtrator.read_rds_table('legacy_users')).phone_number)
-# print(df)
 
